40316fd1caa8cf889c3206410d1130fe782a7855
Andre Nanninga <andre.nanninga@buyways.nl>
2015-03-23 16:54:38 +0100

    moved some stuff around

--- gulpfile.js
+++ gulpfile.js
@@ -9,27 +9,32 @@ var uglify      = require('gulp-uglify');
 
 gulp.task('webserver', function() {
   connect.server({
-    root: [__dirname],
+    root: ['./build'],
     livereload: true
   });
 });
 
 gulp.task('browserify', function() {
-  gulp.src('./logo.js')
+  gulp.src('./src/index.js')
     .pipe(plumber())
     .pipe(browserify())
     //.pipe(uglify())
-    .pipe(rename('logo.min.js'))
-    .pipe(gulp.dest('.'))
+    .pipe(rename('index.js'))
+    .pipe(gulp.dest('./build'))
     .pipe(connect.reload());
 });
 
+gulp.task('copy', function() {
+  gulp.src('./src/index.html')
+    .pipe(plumber())
+    .pipe(gulp.dest('./build'))
+    .pipe(connect.reload());
+})
+
 gulp.task('watch', function() {
-  gulp.watch('./logo.js', ['browserify']);
-  gulp.watch('./index.html', function() {
-    connect.reload();
-  });
+  gulp.watch('./src/*.js', ['browserify']);
+  gulp.watch('./src/index.html', ['copy']);
 });
 
-gulp.task('build', ['browserify']);
+gulp.task('build', ['browserify', 'copy']);
 gulp.task('default', ['build', 'webserver', 'watch']);
